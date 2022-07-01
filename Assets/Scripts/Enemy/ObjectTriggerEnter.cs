ad484e3de38b9fb7bcd23c67edae4364aaaa4087
Kashfornoriginal <Kashfornoriginal@mail.ru>
2022-07-01 05:57:36 -0700

    Add-Random-Position-Generator

--- Assets/Scripts/Enemy/ObjectTriggerEnter.cs
+++ Assets/Scripts/Enemy/ObjectTriggerEnter.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 
 public class ObjectTriggerEnter : MonoBehaviour
 {
+    [SerializeField] private RandomPositionMover _randomPositionMover;
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.TryGetComponent(out Enemy enemy))
         {
-            
+            _randomPositionMover.MoveToGeneratedPosition(collider.gameObject);
         }
     }
 }
--- Assets/Scripts/Enemy/RandomPositionMover.cs
+++ Assets/Scripts/Enemy/RandomPositionMover.cs
@@ -1,10 +1,17 @@
-using System;
 using UnityEngine;
 
-public class RandomPositionGenerator : MonoBehaviour
+public class RandomPositionMover : MonoBehaviour
 {
-    public void GeneratePosition(Collider collider)
+    System.Random random = new System.Random();
+    
+    public void MoveToGeneratedPosition(GameObject gameObject)
     {
-        
+        gameObject.transform.position = RandomPositionGenerator();
+    }
+
+    public Vector3 RandomPositionGenerator()
+    {
+        Vector3 _position = new Vector3((random.Next(-155,155)/100),0.5f,45);
+        return _position;
     }
 }
