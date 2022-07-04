41c35bb232e38b765b72ceca4e7119a1c3aca9bd
Kashfornoriginal <Kashfornoriginal@mail.ru>
2022-07-04 00:11:28 -0700

    Add-Some-Settings-For-Random-Position-Mover

--- Assets/Scripts/Objects/RandomPositionMover.cs
+++ Assets/Scripts/Objects/RandomPositionMover.cs
@@ -2,8 +2,17 @@ using UnityEngine;
 
 public class RandomPositionMover : MonoBehaviour
 {
-    [SerializeField] private int minX;
-    [SerializeField] private int maxX;
+    [Header("Min and max points of road")]
+    [SerializeField] private int _minX;
+    [SerializeField] private int _maxX;
+    
+    [Header("Height of object on road")]
+    [SerializeField] private int _height;
+
+    [Header("Start position by Z-axis")]
+    [SerializeField] private int _zPos;
+
+
     System.Random random = new System.Random();
     
     public void MoveToGeneratedPosition(GameObject gameObject)
@@ -13,7 +22,7 @@ public class RandomPositionMover : MonoBehaviour
 
     private Vector3 RandomPositionGenerator()
     {
-        Vector3 _position = new Vector3(random.Next(minX,maxX)/100f,0.5f,45);
+        Vector3 _position = new Vector3(random.Next(_minX,_maxX)/100f,_height,_zPos); // 100f - devide to do float coordinates
         return _position;
     }
 }
