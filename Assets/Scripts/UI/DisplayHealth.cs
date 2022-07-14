28a458a26137fa53e8f340957e4ffc45f671a82d
Kashfornoriginal <Kashfornoriginal@mail.ru>
2022-07-14 03:09:06 -0700

    Add-Hearts-System

--- Assets/Scripts/UI/DisplayHealth.cs
+++ Assets/Scripts/UI/DisplayHealth.cs
@@ -1,10 +1,14 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
 public class DisplayHealth : MonoBehaviour
 {
     [SerializeField] private Player _player;
-    [SerializeField] private TMP_Text _healthText;
+    [SerializeField] private GameObject _heartPrefab;
+
+    [SerializeField] private List<GameObject> _hearts = new List<GameObject>();
     
     private void OnEnable()
     {
@@ -18,6 +22,33 @@ public class DisplayHealth : MonoBehaviour
 
     private void OnHealthChanged(int health)
     {
-        _healthText.text = health.ToString();
+        if (_hearts.Count < health)
+        {
+            int _currentCreatableHealthCount = health - _hearts.Count;
+            for (int i = 0; i < _currentCreatableHealthCount; i++)
+            {
+                CreateHeart();
+            }
+        }
+        else if (_hearts.Count > health)
+        {
+            int _currentDeletableHealthCount = _hearts.Count - health;
+            for (int i = 0; i < _currentDeletableHealthCount; i++)
+            {
+                DestroyHeart(_hearts[_hearts.Count - 1]);
+            }
+        }
+    }
+
+    private void CreateHeart()
+    {
+        GameObject heart = Instantiate(_heartPrefab, new Vector3(0, 0, 0), Quaternion.identity, transform);
+        _hearts.Add(heart);
+    }
+
+    private void DestroyHeart(GameObject gameObject)
+    {
+        Destroy(gameObject);
+        gameObject.toE
     }
 }
